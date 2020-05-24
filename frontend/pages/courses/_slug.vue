b37700967345220c49359332f23a957752bf5245
Matilde Wittrup Enevoldsen <matildeene@outlook.com>
2020-05-24 19:18:49 +0100

    Create a store for the courses. Create _slug.vue to view individual courses. add styling to it

--- frontend/pages/courses/_slug.vue
+++ frontend/pages/courses/_slug.vue
@@ -1,3 +1,65 @@
 <template>
-  <p>ddi</p>
+  <div id="course" v-if="course">
+    <section class="header">
+      <h1>{{ course.title }}</h1>
+      <p>{{ course.description }}</p>
+    </section>
+    <section class="lessons">
+      <container>
+        <template v-if="course.lessons">
+          <a-row type="flex" justify="center" align="middle">
+            <a-col v-for="lesson in this.course.lessons" :key="course.id" :xs="24" :sm="24" :md="24" :lg="8" :xl="8">
+              <card :title="lesson.title" :link="lesson.slug">
+                <p>{{ lesson.description}}</p>
+              </card>
+            </a-col>
+          </a-row>
+        </template>
+      </container>
+    </section>
+  </div>
 </template>
+<script>
+  import {mapState} from 'vuex'
+  import Card from "../../components/Card";
+  import container from "../../components/Container";
+
+  export default {
+    components: {Card, container},
+    mounted() {
+      this.$store.dispatch('courses/getCourse', this.$route.params.slug)
+    },
+    computed: {
+      ...mapState({
+        course: state => state.courses.course
+      })
+    }
+  }
+</script>
+<style>
+  #course .header {
+    display: flex;
+    flex-direction: column;
+    height: 60vh;
+    justify-content: center;
+    align-items: center;
+    background-color: #1890ff;
+    padding: 5px;
+    margin-bottom: 15px;
+  }
+
+  #course .header h1 {
+    font-size: 36px;
+    color: #fff;
+  }
+
+  #course .header h1, #course .header p {
+    text-align: center;
+  }
+
+  #course .header p {
+    margin-top: 15px;
+    font-size: 20px;
+    color: #fefcfc;
+  }
+</style>
--- frontend/store/courses.js
+++ frontend/store/courses.js
@@ -1,10 +1,14 @@
-export const state = () => {
-  courses: []
-}
+export const state = () => ({
+  courses: [],
+  course: []
+})
 
 export const mutations = {
   SET_COURSES(state, value) {
     state.courses = value
+  },
+  SET_COURSE(state, value) {
+    state.course = value
   }
 }
 
@@ -12,6 +16,11 @@ export const actions = {
   async get({commit}) {
     await this.$axios.$get('/api/course/all').then(function (resp) {
       commit('SET_COURSES', resp.data.courses)
+    })
+  },
+  async getCourse({commit}, slug) {
+    await this.$axios.$get('/api/courses/' + slug).then(function (resp) {
+      commit('SET_COURSE', resp.data)
     })
   }
 }
